3641e94379651117512dce76f3d711674737bb99
Mark Gibbs <mark@gibbs.consulting>
2018-07-10 14:26:43 -0700

    Removed DPDirect component

--- src/index.js
+++ src/index.js
@@ -1,9 +1,6 @@
 /* eslint-disable import/prefer-default-export */
-import DPDirectComponent from './components/DPDirectComponent.react';
 import Pipe from './components/Pipe.react';
 
 export {
-    DPDirectComponent,
-
     Pipe
 };
